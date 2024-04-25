0c2b8cf140a4f373ef8869dcddd077a023e1e24e
Alex Huang <alexh16@circinus-19.ics.uci.edu>
2024-04-24 18:57:52 -0700

    add stats.py

---
+++ stats.py
@@ -0,0 +1,41 @@
+import pickle
+from collections import Counter
+
+def print_stats():
+    try:
+        with open("visited.pkl", 'rb') as file:
+            visited_urls = pickle.load(file)
+            print(f"Number of unique pages: {len(visited_urls)}")
+            print()
+        with open("stats.pkl", 'rb') as file:
+            existing_stats = pickle.load(file)
+
+            unique_pages = existing_stats.get('unique_pages_map', {})
+
+            print("Unique Pages Map:")
+            if unique_pages:
+                for word, count in unique_pages.items():
+                    print(f"{word}, {count}")
+            print()
+
+            print("Longest Page URL:")
+            print(existing_stats.get('longest_page_url', 'Not found'))
+            print()
+
+            print("Longest Page Words:")
+            print(existing_stats.get('longest_page_words', 0))
+            print()
+
+            # Print the 50 most common words
+            print("Top 50 Most Common Words:")
+            word_dict = existing_stats.get('word_dict', {})
+            if word_dict:
+                common_words_counter = Counter(word_dict)
+                top_50_words = common_words_counter.most_common(50)
+                for word, count in top_50_words:
+                    print(f"{word}: {count}", end=', ')
+            print()
+    except FileNotFoundError:
+        print("No statistics found. Run 'update_statistics' function first.")
+
+print_stats()
